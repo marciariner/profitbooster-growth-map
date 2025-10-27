9ee18b9a5e4014bde2c4754fb3eabc0385a7dfc8
marciariner <marcia@infiniteprofitconsulting.com>
2025-10-26 19:06:20 -0700

    Update ping.ts

--- api/ping.ts
+++ api/ping.ts
@@ -3,6 +3,6 @@ export const config = { runtime: 'edge' };
 export default async function handler() {
   return new Response(JSON.stringify({ ok: true, time: new Date().toISOString() }), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' }
+    headers: { "Content-Type": "application/json" }
   });
 }
